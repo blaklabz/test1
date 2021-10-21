1e2a2df27c369cd1118bc6079836ff612a4944d7
blaklabz <90217527+blaklabz@users.noreply.github.com>
2021-10-21 15:02:39 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-instance"
+  ami                    = "ami-0747bdcabd34c712a"
+  instance_type          = "t2.micro"
+  key_name               = "cloud-blak2"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-08e3d7a49bf79c04c"]
+  subnet_id              = "subnet-00b9a43a481212d4a"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
